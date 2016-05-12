c09596e3e83a1b410977c65108babfad3c4cb2a9
Constantin S. Pan <kvapen@gmail.com>
2016-05-12 21:17:53 +0300

    Implement first paragraph extraction. Make UI nicer.

--- src/dctnr/serve.go
+++ src/dctnr/serve.go
@@ -4,7 +4,11 @@ import (
 	"time"
 	"fmt"
 	"strings"
+	"golang.org/x/net/html"
+	"bytes"
 	"net/http"
+	"net/url"
+	"encoding/json"
 	"github.com/gocraft/web"
 	"dctnr/db"
 )
@@ -33,12 +37,170 @@ func (c *Context) SayHello(rw web.ResponseWriter, req *web.Request) {
 	fmt.Println(strings.Repeat("Hello ", c.HelloCount), "World! ")
 }
 
+func deepGet(obj interface{}, path ...interface{}) (interface{}, bool) {
+	var val interface{} = obj
+
+	for _, keystr := range path {
+		if key, ok := keystr.(int); ok {
+			if arr, ok := val.([]interface{}); ok {
+				val = arr[key];
+			} else {
+				return val, false;
+			}
+		} else if key, ok := keystr.(string); ok {
+			if m, ok := val.(map[string]interface{}); ok {
+				val = m[key];
+			} else {
+				return val, false;
+			}
+		}
+	}
+
+	return val, true
+}
+
+type LL struct {
+	Lang      string `json:"lang"`
+	URL       string `json:"url"`
+	Langname  string `json:"langname"`
+	Autonym   string `json:"autonym"`
+	Autotitle string `json:"*"`
+}
+
+type LLPage struct {
+	Pageid    int    `json:"pageid"`
+	Title     string `json:"title"`
+	Langlinks []LL   `json:"langlinks"`
+}
+
+type LLResponse struct {
+	Query struct {
+		Normalized struct {
+			From string            `json:"from"`
+			To   string            `json:"to"`
+		} `json:"normalized"`
+		Pages        map[string]LLPage `json:"pages"`
+	} `json:"query"`
+}
+
+//func fetchArticle(url string) (io.Reader, error) {
+//	res, err := http.Get(url)
+//	if err != nil {
+//		return nil, err
+//	}
+//	return res.Body, nil
+//}
+
+func getElementById(node *html.Node, id string) *html.Node {
+	if node.Type == html.ElementNode {
+		for _, a := range node.Attr {
+			if a.Key == "id" && a.Val == id {
+				return node
+			}
+		}
+	}
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		if found := getElementById(c, id); found != nil {
+			return found
+		}
+	}
+	return nil
+}
+
+func getChildByTag(node *html.Node, tag string) *html.Node {
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.ElementNode && c.Data == "p" {
+			return c
+		}
+	}
+	return nil
+}
+
+func renderNode(node *html.Node) (string, error) {
+	buf := new(bytes.Buffer)
+	if err := html.Render(buf, node); err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
+
+func getFirstParagraph(url string) (string, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+
+	root, err := html.Parse(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	if c := getElementById(root, "mw-content-text"); c != nil {
+		if p := getChildByTag(c, "p"); p != nil {
+			return renderNode(p)
+		}
+	}
+
+	return "", nil
+}
+
+func fetchLanguages(phrase string, src string, dst string) (string, int) {
+	q := url.Values{}
+	q.Set("action", "query")
+	q.Set("prop", "langlinks")
+	q.Set("format", "json")
+	q.Set("lllang", dst)
+	q.Set("llprop", "url|langname|autonym")
+	q.Set("titles", phrase)
+
+	url := fmt.Sprintf(
+		"https://%s.wikipedia.org/w/api.php?%s",
+		src, q.Encode(),
+	)
+
+	fmt.Println("getting " + url)
+
+	res, err := http.Get(url)
+	if err != nil {
+		return "something went wrong", 500
+	}
+
+	code := res.StatusCode
+
+	if code != 200 {
+		return "wrong return code during article fetching", code
+	}
+
+	dec := json.NewDecoder(res.Body)
+	var resp LLResponse
+	err = dec.Decode(&resp);
+	if err != nil {
+		return "something went wrong during response decoding", 500
+	}
+
+	var result string
+
+	for _, page := range resp.Query.Pages {
+		for _, link := range page.Langlinks {
+			p, err := getFirstParagraph(link.URL)
+			if err != nil {
+				result += "\nerror"
+			}
+			result += "\n" + p
+		}
+	}
+
+	return result, code
+}
+
 func (c *Context) Search(rw web.ResponseWriter, req *web.Request) {
-	phrase := req.URL.Query().Get("phrase");
-	rw.WriteHeader(http.StatusOK);
-	reply := "your phrase was: '" + phrase + "'";
-	fmt.Println(reply);
-	rw.Write([]byte(reply));
+	q := req.URL.Query()
+	phrase := q.Get("phrase")
+	src := q.Get("src")
+	dst := q.Get("dst")
+	reply, code := fetchLanguages(phrase, src, dst)
+	rw.WriteHeader(code)
+	rw.Write([]byte(reply))
 }
 
 func (c *Context) Stats(rw web.ResponseWriter, req *web.Request) {
--- static/dctnr.js
+++ static/dctnr.js
@@ -1,6 +1,7 @@
 var veil = document.getElementById('veil');
 var input = document.getElementById('input');
 var output = document.getElementById('output');
+var header = document.getElementById('header');
 
 var xhttp = undefined;
 
@@ -20,8 +21,10 @@ function get(url, c_ok, c_fail) {
 	xhttp.send();
 }
 
-function search(phrase, c_ok, c_fail) {
-	var url = '/search?phrase=' + encodeURIComponent(phrase);
+function search(phrase, src, dst, c_ok, c_fail) {
+	var url = 'search?phrase=' + encodeURIComponent(phrase);
+	url += '&src=' + encodeURIComponent(src);
+	url += '&dst=' + encodeURIComponent(dst);
 	console.log('querying ' + url);
 	get(url, c_ok, c_fail);
 }
@@ -29,16 +32,22 @@ function search(phrase, c_ok, c_fail) {
 function handleSubmit(evt) {
 	evt.preventDefault();
 	var phrase = document.getElementById('input-phrase').value;
+	var src = document.getElementById('input-src').value;
+	var dst = document.getElementById('input-dst').value;
 	output.innerHTML = '';
 	veil.style.display = 'block';
-	search(phrase, function(data) {
+	search(phrase, src, dst, function(data) {
 		console.log('succeeded');
 		output.innerHTML = data;
+		header.classList.remove('maximized');
 		veil.style.display = 'none';
+		output.style.display = 'block';
 	}, function(data) {
 		console.log('failed');
 		output.innerHTML = data;
+		header.classList.remove('maximized');
 		veil.style.display = 'none';
+		output.style.display = 'block';
 	});
 }
 
